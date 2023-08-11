10baf04d094d43523a267a5a8e9b3db2e22ade0d
VictoorLDS <vlds221005@gmail.com>
2023-08-11 11:11:49 -0300

    criação pasta models

--- livraria/models/livro.py
+++ livraria/models/livro.py
@@ -1,28 +1,7 @@
 from django.db import models
 
-class Categoria(models.Model):
-    descricao = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return self.descricao
+from livraria.models import Autor, Categoria, Editora
 
-class Editora(models.Model):
-    nome = models.CharField(max_length=100)
-    site = models.URLField(null=True, blank=True)
-    
-    def __str__(self):
-        return self.nome
-
-class Autor(models.Model):
-    nome = models.CharField(max_length=255)
-    email = models.EmailField(null=True, blank=True)
-
-    def __str__(self):
-        return self.nome
-
-    class Meta:
-        verbose_name = "Autor"
-        verbose_name_plural = "Autores"
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
