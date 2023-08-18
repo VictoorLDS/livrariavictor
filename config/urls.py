aded9c7b79c30411455321bfcd565c2810ba7211
VictoorLDS <vlds221005@gmail.com>
2023-08-18 10:23:55 -0300

    aula 15

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from usuario.router import router as usuario_router
+
 from rest_framework.routers import DefaultRouter
 
 from livraria.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet
@@ -14,4 +16,5 @@ router.register(r"livros", LivroViewSet)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+    path("api/", include(usuario_router.urls)),
 ]
